616372edd7a74403d12c24e96ca6fca315aef728
Enesi George <georgeenesi81@gmail.com>
2024-04-27 15:01:31 +0100

    updating endpoints, resolving bugs

--- account/views.py
+++ account/views.py
@@ -1,50 +1,95 @@
+import logging
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
 from djoser.views import UserViewSet as DjoserUserViewSet
-from djoser.email import PasswordResetEmail as DjoserPasswordResetEmail
-
+from .permissions import *
 from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
-from os import path
-from .permissions import IsAdminOrReadOnly, IsAdmin, IsSuperUser, IsOwner, IsOwnerOrReadOnly, CanCreateSuperAdmin
+from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView
 from .models import User
 from .serializers import *
-from urllib3.util import parse_url
-from Eshop.settings import BASE_DIR, DEFAULT_APP_URL
+
+
 # Create your views here.
-
+# ordinary user registration
 class UserViewSet(DjoserUserViewSet):
-	def perform_update(self, serializer):
-		super().perform_update(serializer)
-class PasswordResetEmail(DjoserPasswordResetEmail):
-	template_name = path.join(BASE_DIR,  "templates/emails/password_reset.html")
-	def get_context_data(self):
-        # PasswordResetEmail can be deleted
-		context = super().get_context_data()
-		url = parse_url(DEFAULT_APP_URL)
-		context["domain"] = url.hostname
-		context["protocol"] = url.scheme
-		print(f"{context['protocol']}{DEFAULT_APP_URL}")
-		return context
-		
+    def perform_update(self, serializer):
+        try:
+            super().perform_update(serializer)
+        except Exception as e:
+            logging.exception("An error has occur while creating user")
 
-class AdminUserView(GenericAPIView):
-    """ A view for creating Admin users """
+
+# admin user registration
+class AdminUserView(DjoserUserViewSet):
+    """A view for creating, retrieving, updating, and deleting users (Admin-only)"""
+
     queryset = User.objects.all()
-    serializer_class = AdminUserCreateSerializer
+    serializer_class_create = AdminUserCreateSerializer  # create admin user
+    serializer_class_retrieve = UserSerializer  # return this serializer afte creation
 
-    def post(self, request):
-        data = {**request.data, "role": "admin"}
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        print("I'm here")
-        user = serializer.save()
+    def get_serializer_class(self):
+        try:
+            if self.action in ["list", "retrieve"]:
+                return self.serializer_class_retrieve
+            elif self.action == "create":
+                return self.serializer_class_create
+            return super().get_serializer_class()
+        except Exception as e:
+            logging.exception(
+                "An error has occur while performing operation on  the admin view."
+            )
+            return Response(
+                {"message": "An error has occur. Please contact the administrator"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
-        
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class UserListAndDetail(ListAPIView, RetrieveAPIView, DestroyAPIView):
+    permission_classes = [IsAdmin]
+    serializer_class = UserSerializer
+
+    def get(self, request, pk=None):
+        try:
+            if pk:
+                try:
+                    user = User.objects.get(pk=pk)
+                    serializer = self.serializer_class(user)
+                    return Response(serializer.data)
+                except User.DoesNotExist:
+                    return Response(
+                        {"message": "User not found."}, status=status.HTTP_404_NOT_FOUND
+                    )
+            else:
+                queryset = User.objects.all()
+                serializer = self.serializer_class(queryset, many=True)
+                return Response(serializer.data)
+        except Exception as e:
+            logging.exception("An Error while fetching the users detail")
+            return Response(
+                {"messgae": "An error has occur. Please contact the administrator"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    def delete(self, request, pk=None):
+        try:
+            if pk:
+                try:
+                    user = User.objects.get(pk=pk)
+                    user.delete()
+                    return Response(
+                        {"message": "User deleted successfully"},
+                        status=status.HTTP_200_OK,
+                    )
+                except User.DoesNotExist:
+                    return Response(
+                        {"message": "User not found."}, status=status.HTTP_404_NOT_FOUND
+                    )
+        except Exception as e:
+            logging.exception("An Error occur while deleting the users")
+            return Response(
+                {"messgae": "An error has occur. Please contact the administrator"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
